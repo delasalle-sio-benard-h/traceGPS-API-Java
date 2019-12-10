7ed07fcc8d4dd18d1308891bbdb4445edf596c4d
delasalle-sio-benard-h <delasallesio.benard.h@gmail.com>
2019-12-10 13:50:37 +0100

    lkljl

--- src/jim/testsvisuels/TestPasserelleServicesWebXML.java
+++ src/jim/testsvisuels/TestPasserelleServicesWebXML.java
@@ -1,5 +1,7 @@
 package jim.testsvisuels;
 
+import static org.junit.Assert.assertEquals;
+
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,6 +17,16 @@ public class TestPasserelleServicesWebXML {
 	public static void main(String[] args) throws ParseException {
 		
 		String msg;
+		
+		// test visuel de la méthode connecter
+		msg = PasserelleServicesWebXML.connecter("admin", "adminnnnnnnn");
+		System.out.println(msg);
+		
+		msg = PasserelleServicesWebXML.connecter("admin", Outils.sha1("mdpadmin"));
+		System.out.println(msg);
+		
+		msg = PasserelleServicesWebXML.connecter("europa", Outils.sha1("mdputilisateur"));
+		System.out.println(msg);
 	
 		// test visuel de la méthode getTousLesUtilisateurs
 
